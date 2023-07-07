f3876c5f5570358b3c44f329a222f2bab73210ec
floralrainfall <waternewgatchi@gmail.com>
2023-07-07 04:14:38 -0400

    remove leftover

--- src/client/player/main.cpp
+++ src/client/player/main.cpp
@@ -1,15 +1,6 @@
 #include <stdio.h>
 
-#include <QApplication>
-
-#include <client/player/PlayerWindow.hpp>
-
 int main(int argc, char** argv)
 {
-    QApplication app(argc, argv);
-    PlayerWindow window = PlayerWindow();
-    
-    window.show();
-
-    return app.exec();
+    printf("Hi\n");
 }
