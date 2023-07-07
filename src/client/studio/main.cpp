b9a885791ce1c7a785bcdbb99e17c09c92430d92
floralrainfall <waternewgatchi@gmail.com>
2023-07-07 17:45:34 -0400

    uncapped framerate

--- src/client/studio/main.cpp
+++ src/client/studio/main.cpp
@@ -6,6 +6,12 @@
 
 int main(int argc, char** argv)
 {
+    QSurfaceFormat format;
+    format.setSwapBehavior(QSurfaceFormat::DoubleBuffer);
+    format.setSwapInterval(0);
+
+    QSurfaceFormat::setDefaultFormat(format);
+
     QApplication app(argc, argv);
     StudioWindow window = StudioWindow();
     
