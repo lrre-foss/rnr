5fb027ea829c00ceb7e8e19477adfaf140acf037
floralrainfall <waternewgatchi@gmail.com>
2023-07-07 01:29:30 -0400

    fix guid and add world

--- src/engine/network/Guid.cpp
+++ src/engine/network/Guid.cpp
@@ -20,7 +20,15 @@ void RBX::Guid::generateGUID(std::string* result)
     memset(sz, 0, sizeof(sz));
     StringFromGUID2(&pguid)
     WideCharToMultiByte(0, 0, sz, 64, MultiByteStr, 64, 0, 0);
+
+    // construct guid
+    result = "RBX";
     result += MultiByteStr;
+    result->erase(40, 1);
+    result->erase(27, 1);
+    result->erase(22, 1);
+    result->erase(17, 1);
+    result->erase(3, 1);
 #else 
     // TBD: POSIX method
 #endif
