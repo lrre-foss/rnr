a158d87b63941a5943a1ba79e953befeed17ba88
floralrainfall <waternewgatchi@gmail.com>
2023-07-07 03:48:43 -0400

    icons embed in their programs

--- src/client/studio/StudioWindow.cpp
+++ src/client/studio/StudioWindow.cpp
@@ -1,9 +1,14 @@
 #include <client/studio/StudioWindow.hpp>
+#include "studio_rsc.h"
 
 StudioWindow::StudioWindow()
 {
     setWindowTitle(QString("RBXNU Studio"));
-    setWindowIcon(QIcon(":/content/images/icon.png"));
+
+    QPixmap icon_pixmap = QPixmap();
+    icon_pixmap.loadFromData(icon_png, icon_png_size);
+
+    setWindowIcon(QIcon(icon_pixmap));
 }
 
 void StudioWindow::closeEvent(QCloseEvent* event)
