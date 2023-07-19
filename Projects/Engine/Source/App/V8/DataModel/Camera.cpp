203dc236a90eabffdc942e633b94619b22929a95
floralrainfall <waternewgatchi@gmail.com>
2023-07-18 23:01:40 -0400

    fix naming mistake

--- Projects/Engine/Source/App/V8/DataModel/Camera.cpp
+++ Projects/Engine/Source/App/V8/DataModel/Camera.cpp
@@ -34,7 +34,7 @@ namespace RNR
         Ogre::Radian old_pitch;
         Ogre::Radian old_yaw;
         Ogre::Radian old_roll;
-        
+
         getCFrame().getRotation().ToEulerAnglesZXY(old_yaw, old_pitch, old_roll);
 
         pitch = old_pitch + pitch;
@@ -52,7 +52,7 @@ namespace RNR
               ACCESS_NONE, OPERATION_READWRITE, PROPERTY_CFRAME,         
               REFLECTION_GETTER(Camera* instance = (Camera*)object; return &instance->m_cframe; ), 
               REFLECTION_SETTER(Camera* instance = (Camera*)object; instance->setCFrame(*(CoordinateFrame*)value); ) },
-            { this, std::string("Target"), std::string(""), 
+            { this, std::string("Focus"), std::string(""), 
               ACCESS_NONE, OPERATION_READWRITE, PROPERTY_CFRAME,         
               REFLECTION_GETTER(Camera* instance = (Camera*)object; return &instance->m_focus; ), 
               REFLECTION_SETTER(Camera* instance = (Camera*)object; instance->setFocus(*(CoordinateFrame*)value); ) },
