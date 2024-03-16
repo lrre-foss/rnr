126c540e31352817098c3950cbd6f6c40766d4d2
floralrainfall <waternewgatchi@gmail.com>
2024-03-16 12:55:52 -0400

    add basic window

--- client/Client.cpp
+++ client/Client.cpp
@@ -4,6 +4,37 @@
 
 int main(int argc, char **argv) {
   int sr = SDL_Init(SDL_INIT_VIDEO);
-  if (sr)
+  if (sr) {
     DEV_LOGMSGF("SDL_Init returned %s", SDL_GetError());
+    return -1;
+  }
+
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+  SDL_Window* window = SDL_CreateWindow("RNR", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+  if(!window) {
+    DEV_LOGMSGF("SDL_CreateWindow returned %s", SDL_GetError());
+    return -1;
+  }
+  
+  SDL_GLContext glcontext = SDL_GL_CreateContext(window);
+  if(!glcontext) {
+    DEV_LOGMSGF("SDL_GL_CreateContext returned %s", SDL_GetError());
+    return -1;
+  }
+
+  bool running = true;
+  while(running) {
+    SDL_Event event;
+    while(SDL_PollEvent(&event)) {
+      if(event.type == SDL_QUIT)
+        running = false;
+    }
+
+    SDL_GL_SwapWindow(window);
+  }
+
+  DEV_LOGMSGF("goodbye");
 }
